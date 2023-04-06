64b0825c96d59af254a697eb102058f268c05608
EC2 Default User <ec2-user@ip-172-31-83-71.ec2.internal>
2023-04-06 01:03:58 +0000

    funcao

---
+++ funcao/funcao14.py
@@ -0,0 +1,14 @@
+# palindromo - palavra ou frase que se pode ler,
+# indiferentemente, da esquerda para a
+# direita ou vice-versa.
+def verificar_palindromo(texto):
+    texto = texto.lower().replace(" ", "")
+    return texto == texto [::-1]
+    
+# texto[::-1] inverte o texto
+
+texto = "Socorram-me, subi no ônibus em Marrocos"
+if verificar_palindromo(texto):
+    print(texto, "é um palíndromo.")
+else:
+    print(texto, "não é um palíndromo.")
---
+++ funcao/funcao5.py
@@ -0,0 +1,6 @@
+def funcao_detectada(n):
+    if(n%2==0):
+        return "É par"
+    else:
+        return "É impar"
+print(funcao_detectada(int(input('Entre com um número:'))))
